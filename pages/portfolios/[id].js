ab1919a280e8eb41508886ac1afc71b354769be3
nonusae <nonusae@gmail.com>
2020-12-19 21:57:29 +0700

    Display portfolio detail.

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,14 +1,31 @@
 import React from 'react';
 import BaseLayout from '../../components/layouts/BaseLayout';
+import axios from 'axios'
 
 import { withRouter } from "next/router";
 
 class Portfolio extends React.Component {
+
+  static async getInitialProps({query}) {
+    let post = [];
+    try {
+      const res = await  axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
+      post = res.data;
+    } catch(e) {
+      console.log(e);
+    }
+
+    return { portfolio: post };
+  }
+
   render () {
+    const { portfolio } = this.props;
     return (
       <BaseLayout>
         <h1>I'm portfolio page</h1>
-        <h2>{this.props.router.query.id}</h2>
+        <h1>{portfolio.title}</h1>
+        <p>Body: {portfolio.body}</p>
+        <p>ID: {portfolio.id}</p>
       </BaseLayout>
     )
   }
