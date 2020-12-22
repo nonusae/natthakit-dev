d643c6170bdeca3e22d5105884326e1f7a195cbe
nonusae <nonusae@gmail.com>
2020-12-22 22:26:51 +0700

    Make post detail page use serverless function

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 export const useGetData = (url) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
 
@@ -10,6 +10,7 @@ export const useGetData = (url) => {
       const res = await fetch(url);
       const result = await res.json();
       if (res.status !== 200) {
+        console.log(result)
         setError(result)
       } else {
         setData(result);
@@ -17,8 +18,8 @@ export const useGetData = (url) => {
       setLoading(false);
     }
 
-    fetchData();
-  }, [])
+    url && fetchData();
+  }, [url])
 
   return {data, error, loading}
 }
--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,31 +1,28 @@
 import BaseLayout from 'components/layouts/BaseLayout';
 import BasePage from 'components/BasePage';
-
 import axios from 'axios'
+import { useGetData } from 'actions';
+import { useRouter } from "next/router";
 
-import { withRouter } from "next/router";
+const Portfolio = () => {
+  const router = useRouter();
+  const { data: portfolio, error, loading } =
+    useGetData(router.query.id ? `/api/v1/posts/${router.query.id}` : null)
 
-const Portfolio = ({portfolio}) => (
-  <BaseLayout>
+    return (<BaseLayout>
     <BasePage>
-      <h1>I'm portfolio page</h1>
-      <h1>{portfolio.title}</h1>
-      <p>Body: {portfolio.body}</p>
-      <p>ID: {portfolio.id}</p>
+      { loading && <span>Loading</span> }
+      { error && <div className="alert alert-danger">{error.message}</div> }
+      { portfolio &&
+        <>
+          <h1>I'm portfolio page</h1>
+          <h1>{portfolio.title}</h1>
+          <p>Body: {portfolio.body}</p>
+          <p>ID: {portfolio.id}</p>
+        </>
+      }
     </BasePage>
-  </BaseLayout>
-)
-
-Portfolio.getInitialProps = async ({query}) => {
-  let post = [];
-  try {
-    const res = await  axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
-    post = res.data;
-  } catch(e) {
-    console.log(e);
-  }
-
-  return { portfolio: post };
+  </BaseLayout>)
 }
 
-export default withRouter(Portfolio);
+export default Portfolio;
