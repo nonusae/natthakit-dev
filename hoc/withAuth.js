e6d1b2c4dd90a6cd3f635ea92567db81274e2376
nonusae <nonusae@gmail.com>
2020-12-26 20:33:38 +0700

    Implement more wihtAuth logic

--- hoc/withAuth.js
+++ hoc/withAuth.js
@@ -1,8 +1,19 @@
-
+import { useGetUser } from 'actions/user';
+import Redirect from 'components/shared/redirect'
 
 const withAuth = (Component) => {
   return props => {
-    return <Component {...props}/>
+    const { data: dataUser, loading: loadingUser } = useGetUser();
+
+    if(loadingUser) {
+      return <p>Loading ...</p>
+    }
+
+    if (!dataUser) {
+      return <Redirect ssr to='/api/v1/login' />
+    } else {
+      return <Component user={dataUser} loading={loadingUser} {...props}/>
+    }
   }
 }
 
--- pages/secret.js
+++ pages/secret.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import BaseLayout from 'components/layouts/BaseLayout'
 import BasePage from 'components/BasePage';
-import { useGetUser } from 'actions/user';
-import Redirect from 'components/shared/redirect'
 import withAuth from 'hoc/withAuth';
 
 
-const Secret = () => {
-  const { data: dataUser, loading: loadingUser } = useGetUser();
-
-  if(loadingUser) {
-    return <p>Loading ...</p>
-  }
-
-  if (!dataUser) {
-    return <Redirect to='/api/v1/login' />
-  } else {
-    return (
-      <BaseLayout user={dataUser} loading={loadingUser}>
-        <BasePage>
-          <div>
-            <h1>
-              This is Secret page
-            </h1>
-          </div>
-        </BasePage>
-      </BaseLayout>
-    )
-  }
+const Secret = ({user, loading}) => {
+  return (
+    <BaseLayout user={user} loading={loading}>
+      <BasePage>
+        <div>
+          <h1>
+            This is Secret page - {user.name}
+          </h1>
+        </div>
+      </BasePage>
+    </BaseLayout>
+  )
 }
 
 export default withAuth(Secret) ;
