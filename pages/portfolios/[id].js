692ea5e53ff259598508868d5540e2e77f5312a8
nonusae <nonusae@gmail.com>
2021-01-01 23:00:10 +0700

    Make portfolio detail page prerendered

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -17,10 +17,23 @@ const Portfolio = ({portfolio}) => {
   )
 }
 
-export async function getServerSideProps({query}) {
-  const json = await new PortfoliosApi().getById(query.id)
-  const portfolio = json.data.data
-  return { props: {portfolio} }
+export async function getStaticPaths() {
+  const json = await new PortfoliosApi().getAll();
+  const portfolios = json.data.data
+
+  const paths = portfolios.map(portfolio => {
+    return {
+      params: {id: portfolio.id}
+    }
+  })
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({params}) {
+  const json = await new PortfoliosApi().getById(params.id)
+  const portfolios = json.data.data;
+  return { props: {portfolios} }
 }
 
 export default Portfolio;
