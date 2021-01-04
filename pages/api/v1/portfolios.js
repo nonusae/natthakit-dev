4bce3bafc63e65eda4270041704b523f2ea6c7cf
nonusae <nonusae@gmail.com>
2021-01-04 19:00:14 +0700

    Implment function for get access token from auth0

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -1,10 +1,11 @@
 import PortfoliosApi from "lib/api/portfolios";
+import auth0 from 'utils/auth0';
 
 export default async function createPortfolio(req, res) {
   try {
-    const data = req.body;
-    await new PortfoliosApi().createPortfolio(data);
-    return res.json({'message': 'Success'})
+    const {accessToken} = await auth0.getSession(req);
+    await new PortfoliosApi().createPortfolio(req.body);
+    return res.json({message: 'Success'})
   } catch (error) {
     return res.status(error.status || 400).end(error.message)
   }
