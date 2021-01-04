baefe37c5b73efcfbda20403313bc1d2cbc2c92d
nonusae <nonusae@gmail.com>
2021-01-04 19:17:53 +0700

    Use access token to create portfolio

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -3,8 +3,8 @@ import auth0 from 'utils/auth0';
 
 export default async function createPortfolio(req, res) {
   try {
-    const {accessToken} = await auth0.getSession(req);
-    await new PortfoliosApi().createPortfolio(req.body);
+    const { accessToken } = await auth0.getSession(req);
+    await new PortfoliosApi(accessToken).createPortfolio(req.body);
     return res.json({message: 'Success'})
   } catch (error) {
     return res.status(error.status || 400).end(error.message)
