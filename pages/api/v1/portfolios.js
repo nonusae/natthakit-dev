10370b4c783cbdd9660b19da20c6fcc36c656369
nonusae <nonusae@gmail.com>
2021-01-04 23:14:23 +0700

    Response with data from server upon portfolio creation

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -4,8 +4,8 @@ import auth0 from 'utils/auth0';
 export default async function createPortfolio(req, res) {
   try {
     const { accessToken } = await auth0.getSession(req);
-    await new PortfoliosApi(accessToken).createPortfolio(req.body);
-    return res.json({message: 'Success'})
+    const json = await new PortfoliosApi(accessToken).createPortfolio(req.body);
+    return res.json(json.data)
   } catch (error) {
     return res.status(error.status || 400).end(error.message)
   }
