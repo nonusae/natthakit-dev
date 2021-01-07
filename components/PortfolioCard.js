e5c504c15dffde5a1f6ac199382d384daed079d4
nonusae <nonusae@gmail.com>
2021-01-07 17:41:17 +0700

    Add edit and delete page for portoflio

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -1,12 +1,14 @@
+import { Children } from 'react';
 import { Card, CardHeader, CardBody, CardText, CardTitle } from 'reactstrap';
 
-const PortfolioCard = ({id, title, jobTitle, location, description}) => {
+const PortfolioCard = ({id, title, jobTitle, location, description, children}) => {
   return <Card className="portfolio-card">
     <CardHeader className="portfolio-card-header">{jobTitle}</CardHeader>
     <CardBody>
       <p className="portfolio-card-city">{location}</p>
       <CardTitle className="portfolio-card-title">{title}</CardTitle>
       <CardText className="portfolio-card-text">{description}</CardText>
+      { children }
     </CardBody>
   </Card>
 }
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,11 +1,12 @@
 import BaseLayout from 'components/layouts/BaseLayout';
 import BasePage from 'components/BasePage';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 import Link from 'next/link';
 import { useRouter } from "next/router";
 import { useGetUser } from 'actions/user';
 import PortfoliosApi from 'lib/api/portfolios';
 import PortfolioCard from 'components/portfolioCard';
+import { isAuthorized } from "utils/auth0";
 
 const Portfolios = ({portfolios}) => {
   const router = useRouter();
@@ -27,7 +28,19 @@ const Portfolios = ({portfolios}) => {
                   router.push('/portfolios/[id]', `/portfolios/${portfolio.attributes.id}`)
                 }}
                 >
-                <PortfolioCard {...portfolio.attributes} />
+                <PortfolioCard {...portfolio.attributes}>
+                  { dataUser && isAuthorized(dataUser, 'admin') &&
+                    <>
+                      <Button className='mr-2' color='warning' onClick={
+                        (e) => {
+                          e.stopPropagation();
+                          router.push('/portfolios/[id]/edit', `/portfolios/${portfolio.attributes.id}/edit`)
+                        }
+                      }>Edit</Button>
+                      <Button color='danger'>Delete</Button>
+                    </>
+                  }
+                </PortfolioCard>
               </Col>)
             })
           }
