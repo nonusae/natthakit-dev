d708bca3dc1dcc21d189c6f5a6b5ba7dc32d7a5b
nonusae <nonusae@gmail.com>
2021-01-12 01:35:14 +0700

    Add some SEO and portfolio detail page

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -1,15 +1,35 @@
 import BaseLayout from 'components/layouts/BaseLayout';
 import BasePage from 'components/BasePage';
 import { useGetUser } from 'actions/user';
+import { formatDate } from 'helpers/functions';
 import PortfoliosApi from "lib/api/portfolios";
 
 const Portfolio = ({portfolio}) => {
   const { data: dataUser, loading: loadingUser } = useGetUser();
 
   return (
-    <BaseLayout user={dataUser} loading={loadingUser}>
-      <BasePage header='Portfolio Detail'>
-        {JSON.stringify(portfolio)}
+    <BaseLayout
+      navClass='transparent'
+      user={dataUser}
+      loading={loadingUser}>
+      <BasePage
+        noWrapper
+        indexPage
+        title={`${portfolio.title} - Filip Jerga`}
+        metaDescription={portfolio.description}>
+        <div className="portfolio-detail">
+          <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <main role="main" class="inner page-cover">
+              <h1 class="cover-heading">{portfolio.title}</h1>
+              <p class="lead dates">{formatDate(portfolio.startDate)} - {formatDate(portfolio.endDate) || 'Present'}</p>
+              <p class="lead info mb-0">{portfolio.jobTitle} | {portfolio.company} | {portfolio.location}</p>
+              <p class="lead">{portfolio.description}</p>
+              <p class="lead">
+                <a href={portfolio.companyWebsite} target="_" class="btn btn-lg btn-secondary">Visit Company</a>
+              </p>
+            </main>
+          </div>
+        </div>
       </BasePage>
     </BaseLayout>
   )
