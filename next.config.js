32f8c38f50cdb48ac64541081451dc14f0f42733
nonusae <nonusae@gmail.com>
2021-01-13 21:47:18 +0700

    Add next image optimization plugin

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-module.exports = {
-  webpack: config => {
-    return config;
-  },
-  env: {
-    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
-  }
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+    [optimizedImages, {
+      /* config for next-optimized-images */
+    }],
+    {
+      webpack: config => {
+        return config;
+      },
+      env: {
+        AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
+      }
+    }
+]);
